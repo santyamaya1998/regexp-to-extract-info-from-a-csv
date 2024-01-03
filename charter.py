930bb105b1574ab68c8ac2361f49295fb0655614
SAC98 <samayacetin@uniminuto.edu.co>
2024-01-02 19:49:21 -0500

    update

--- charter.py
+++ charter.py
@@ -7,13 +7,15 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.savefig('barchart.png')
+  plt.close()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.show()
+  plt.savefig('piechart.png')
+  plt.close()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
